56c701863dccdc6fcb4bec3c58af8088d300315c
Хотленко Алексей <khotlenko@www.mts.ru>
2024-07-09 17:01:00 +0300

    Fix: Switch logic for GroupCell

--- src/ComponentsLibrary.jsx
+++ src/ComponentsLibrary.jsx
@@ -5,7 +5,7 @@ const ComponentLibrary = () => {
     return (
         <div className="bg-mono-200">
             <Button type='primary' size='main'>Лучшие моменты</Button>
-            <div className="grid grid-cols-3 gap-2">
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
                 <GroupCell groupName='Super Best Words' words='12' learningCurve='4' isRaised={true} isStarred={false} />
                 <GroupCell groupName='Super Best Words' words='34' learningCurve='6' isRaised={false} isStarred={false} />
                 <GroupCell groupName='Super Best Words' words='3' learningCurve='2' isRaised={false} isStarred={true} />
--- src/components/GroupCell.jsx
+++ src/components/GroupCell.jsx
@@ -1,62 +1,43 @@
-import { useEffect, useState } from "react"
 import { Icon } from "./Icon"
 import { ProgressBar } from "./ProgressBar"
 
 const GroupCell = ({ groupName, words, learningCurve, isStarred, isRaised }) => {
-    const [type, setType] = useState({})
-
-    useEffect(() => {
-        // Default state params
-        if (!isStarred && !isRaised) {
-            setType({
-                ...type,
-                bg: 'bg-white',
-                iconsBlock:
-                    <div className="flex justify-between">
-                        <Icon type='star' style='fill-none stroke-black opacity-40 hover:opacity-70 stroke-l size-8' />
-                        <Icon type='rise' style='fill-black size-8' />
-                    </div>,
-            })
+  const groupType = () => {
+    switch (true) {
+      case (!isRaised && !isStarred):
+        return {
+            bg: 'bg-white',
+            starIcon: 'fill-none stroke-black opacity-40 hover:opacity-70 stroke-l size-8',
+            raiseIcon: 'fill-black size-8',
         }
-
-        // Raised state params
-        if (!isStarred && isRaised) {
-            setType({
-                ...type,
-                bg: 'bg-lime-200',
-                iconsBlock:
-                    <div className="flex justify-between">
-                        <Icon type='star' style='fill-none stroke-black opacity-40 hover:opacity-70 stroke-l size-8' />
-                        <Icon type='rise' style='fill-black size-8' />
-                    </div>,
-            })
+      case (isRaised && !isStarred):
+        return {
+            bg: 'bg-lime-200',
+            starIcon: 'fill-none stroke-black opacity-40 hover:opacity-70 stroke-l size-8',
+            raiseIcon: 'fill-black size-8',
         }
-
-        // Starred state params
-        if (isStarred && !isRaised) {
-            setType({
-                ...type,
-                bg: 'bg-daisy-200',
-                iconsBlock:
-                    <div className="flex justify-between">
-                        <Icon type='star' style='fill-daisy-400 hover:fill-daisy-300 size-8' />
-                        <Icon type='rise' style='fill-black size-8' />
-                    </div>,
-            })
+      case (!isRaised && isStarred):
+        return {
+            bg: 'bg-daisy-200',
+            starIcon: 'fill-daisy-400 hover:fill-daisy-300 size-8',
+            raiseIcon: 'fill-black size-8',
         }
-    }, [isStarred, isRaised])
-
-
-    return (
-        <div className={`${type.bg} py-5 px-4 rounded-3xl space-y-2`}>
-            {type.iconsBlock}
-            <div className="space-y-1 h-20">
-                <div className="font-stwSemibold text-t-subsection">{groupName}</div>
-                <div className="font-stwRegular text-t-default ">{`${words} words`}</div>
-            </div>
-            <ProgressBar size='8' learningCurve={learningCurve} />
-        </div>
-    )
+    }
+  }
+  
+  return (
+    <div className={`${groupType().bg} py-5 px-4 rounded-3xl space-y-2`}>
+      <div className="flex justify-between">
+        <Icon type='star' style={groupType().starIcon} />
+        {!isStarred && <Icon type='rise' style={groupType().raiseIcon} />}
+      </div>
+      <div className="space-y-1 h-20">
+        <div className="font-stwSemibold text-t-subsection">{groupName}</div>
+        <div className="font-stwRegular text-t-default ">{`${words} words`}</div>
+      </div>
+      <ProgressBar isDefault={!isRaised && !isStarred} learningCurve={learningCurve} />
+    </div>
+  )
 }
 
 export { GroupCell }
